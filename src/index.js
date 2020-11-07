2d541f39c4461d69eb0ed5caa0eb66a53ff81b3d
Joaquim Neto <joaquimmy@yahoo.com>
2020-11-07 10:20:44 -0300

    feat!: change function signature, add 404 option

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const { Router } = require('express');
+const root = require('app-root-path');
+const path = require('path');
 
 const { requireFiles, getRoutes } = require('./functions');
 
-async function loadServices(serviceDir, app) {
+async function ponaserv(app, options = {}) {
+  const serviceDir = options.services || path.join(root, 'services');
+  const e404 =
+    options[404] && typeof options[404] === 'function'
+      ? options[404]
+      : (req, res) => res.status(404).json({ message: 'Not found' });
+
   const services = await requireFiles(serviceDir);
 
   for (const service of services) {
@@ -13,9 +21,10 @@ async function loadServices(serviceDir, app) {
     if (!routes.length) continue;
 
     const router = Router();
-    routes.forEach(route => router[route.method](...route.args));
+    routes.forEach((route) => router[route.method](...route.args));
+    router.use(e404);
     app.use(router);
   }
 }
 
-module.exports = loadServices;
+module.exports = ponaserv;
